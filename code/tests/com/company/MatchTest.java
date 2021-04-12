d04c010c1e86af64ec7d2f8a0f12b06dc353a586
Inga-Helgadottitr <78418864+Inga-Helgadottitr@users.noreply.github.com>
2021-04-12 08:34:37 +0200

    Update MatchTest.java

--- code/tests/com/company/MatchTest.java
+++ code/tests/com/company/MatchTest.java
@@ -11,14 +11,19 @@ class MatchTest {
     Match match = new Match(teams);
 
     @Test
-    void giveWinnerTeamPoints() {
+    void whoWon() {
         match.setTeam1Goals(10);
         match.setTeam2Goals(15);
-        match.giveWinnerTeamPoints();
+        match.whoWon();
         int t1Goals = t1.getGoalsMade();
         int t2Goals = t2.getGoalsMade();
+        int t1OGoals = t1.getOpposingTeamsGoals();
+        int t2OGoals = t2.getOpposingTeamsGoals();
 
         assertEquals(10, t1Goals);
         assertEquals(15, t2Goals);
+
+        assertEquals(15, t1OGoals);
+        assertEquals(10, t2OGoals);
     }
 }
