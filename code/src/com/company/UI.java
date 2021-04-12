908ba8423ede3b5d9121ed9139c5a628fda9a049
Angeliza-Medina <78863493+Angeliza-Medina@users.noreply.github.com>
2021-04-12 11:53:36 +0200

    Add a bunch of code

--- code/src/com/company/UI.java
+++ code/src/com/company/UI.java
@@ -1,18 +1,19 @@
 package com.company;
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class UI {
-   Scanner scanner = new Scanner(System.in);
 
    public void displayMsg(String msg){
       System.out.println(msg);
    }
 
    public String getUserInput(String msg){
+      Scanner scanner = new Scanner(System.in);
       System.out.println(msg);
-
       String input = scanner.nextLine();
-
       return input;
    }
+
 }
